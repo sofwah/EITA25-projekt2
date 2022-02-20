57e9769d64024ad88c319340ae82d168b744dc69
sofwah <sofia.wahlmark@gmail.com>
2022-02-20 13:47:28 +0100

    Lagt till abstrakt personklass och subklasser

---
+++ server/Nurse.java
@@ -0,0 +1,22 @@
+package src.server;
+
+import java.util.HashSet;
+
+public class Nurse extends Person {
+    private HashSet<Patient> patients;
+    private String division;
+
+    public Nurse(String name, String division) {
+        super(name);
+        patients = new HashSet<>();
+        this.division = division;
+    }
+
+    public HashSet<Patient> getPatients() {
+        return patients;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+}
---
+++ server/Patient.java
@@ -0,0 +1,8 @@
+package src.server;
+
+public class Patient extends Person {
+
+    public Patient(String name) {
+        super(name);
+    }
+}
